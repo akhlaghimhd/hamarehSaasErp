a1fdbe6056af678eaa17baf6496ef6265bf569b8
akhlaghimhd <77932814+akhlaghimhd@users.noreply.github.com>
2026-09-04 09:26:43 +0330

    L6-INV-MIG-06: Add Inventory routes for Item and Warehouse + remove from MasterData routes

--- app/Modules/Inventory/Routes/api.php
+++ app/Modules/Inventory/Routes/api.php
@@ -1,15 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Modules\Inventory\Controllers\ItemController;
+use App\Modules\Inventory\Controllers\WarehouseController;
 
 /*
 |--------------------------------------------------------------------------
 | Inventory Module API Routes
 |--------------------------------------------------------------------------
-|
-| All routes for the Inventory & Logistics module.
-| Prefix and middleware are applied by the ModuleServiceProvider.
-|
+| Prefix پیش‌فرض توسط ModuleServiceProvider: /api/inventory
 */
 
-// Routes will be registered here after models and controllers are moved.
+Route::middleware(['auth:sanctum', 'tenant.context', 'load.scopes'])->group(function () {
+
+    // Items (Single Source of Truth now in Inventory)
+    Route::get('items', [ItemController::class, 'index'])
+        ->middleware('permission:master-data.item.view');
+    Route::post('items', [ItemController::class, 'store'])
+        ->middleware('permission:master-data.item.create');
+    Route::get('items/{id}', [ItemController::class, 'show'])
+        ->middleware('permission:master-data.item.view');
+    Route::put('items/{id}', [ItemController::class, 'update'])
+        ->middleware('permission:master-data.item.update');
+    Route::delete('items/{id}', [ItemController::class, 'destroy'])
+        ->middleware('permission:master-data.item.delete');
+
+    // Warehouses (Single Source of Truth now in Inventory)
+    Route::get('warehouses', [WarehouseController::class, 'index'])
+        ->middleware('permission:master-data.warehouse.view');
+    Route::post('warehouses', [WarehouseController::class, 'store'])
+        ->middleware('permission:master-data.warehouse.create');
+    Route::get('warehouses/{id}', [WarehouseController::class, 'show'])
+        ->middleware('permission:master-data.warehouse.view');
+    Route::put('warehouses/{id}', [WarehouseController::class, 'update'])
+        ->middleware('permission:master-data.warehouse.update');
+    Route::delete('warehouses/{id}', [WarehouseController::class, 'destroy'])
+        ->middleware('permission:master-data.warehouse.delete');
+
+});
