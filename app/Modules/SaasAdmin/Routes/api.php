07af0be6fd16b2a2178df1515377bf5e87deea97
akhlaghimhd <77932814+akhlaghimhd@users.noreply.github.com>
2026-09-08 06:37:11 +0330

    L2: Wire AdminUser, AdminRole, SystemSetting routes under /api/saas-admin

--- app/Modules/SaasAdmin/Routes/api.php
+++ app/Modules/SaasAdmin/Routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Modules\SaasAdmin\Controllers\AdminUserController;
+use App\Modules\SaasAdmin\Controllers\AdminRoleController;
+use App\Modules\SaasAdmin\Controllers\SystemSettingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,14 +12,72 @@ use Illuminate\Support\Facades\Route;
 | Loaded by ModuleServiceProvider with prefix: /api/saas-admin
 | Middleware 'api' is already applied by the provider.
 |
-| Layer 1 routes (Tenant, Plan, Subscription, Invoice, Addon, Coupon)
-| have been moved to SaasPlatform module.
-| This file is intentionally left minimal until Layer 2 controllers
-| (AdminUser, AdminRole, etc.) are implemented.
+| Layer 1 business routes (Tenant, Plan, Subscription, Invoice, Addon, Coupon)
+| live in SaasPlatform module under /api/saas-platform.
 */
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    // Layer 2 routes will be added here later
-    // Example placeholders (commented):
-    // Route::get('/admin-users', [AdminUserController::class, 'index']);
+
+    // Admin Users
+    Route::get('/admin-users', [AdminUserController::class, 'index'])
+        ->middleware('permission:saas-admin.admin_user.view')
+        ->name('saas-admin.admin-users.index');
+
+    Route::get('/admin-users/{id}', [AdminUserController::class, 'show'])
+        ->middleware('permission:saas-admin.admin_user.view')
+        ->name('saas-admin.admin-users.show');
+
+    Route::post('/admin-users', [AdminUserController::class, 'store'])
+        ->middleware('permission:saas-admin.admin_user.create')
+        ->name('saas-admin.admin-users.store');
+
+    Route::put('/admin-users/{id}', [AdminUserController::class, 'update'])
+        ->middleware('permission:saas-admin.admin_user.update')
+        ->name('saas-admin.admin-users.update');
+
+    Route::delete('/admin-users/{id}', [AdminUserController::class, 'destroy'])
+        ->middleware('permission:saas-admin.admin_user.delete')
+        ->name('saas-admin.admin-users.destroy');
+
+    // Admin Roles
+    Route::get('/admin-roles', [AdminRoleController::class, 'index'])
+        ->middleware('permission:saas-admin.admin_role.view')
+        ->name('saas-admin.admin-roles.index');
+
+    Route::get('/admin-roles/{id}', [AdminRoleController::class, 'show'])
+        ->middleware('permission:saas-admin.admin_role.view')
+        ->name('saas-admin.admin-roles.show');
+
+    Route::post('/admin-roles', [AdminRoleController::class, 'store'])
+        ->middleware('permission:saas-admin.admin_role.create')
+        ->name('saas-admin.admin-roles.store');
+
+    Route::put('/admin-roles/{id}', [AdminRoleController::class, 'update'])
+        ->middleware('permission:saas-admin.admin_role.update')
+        ->name('saas-admin.admin-roles.update');
+
+    Route::delete('/admin-roles/{id}', [AdminRoleController::class, 'destroy'])
+        ->middleware('permission:saas-admin.admin_role.delete')
+        ->name('saas-admin.admin-roles.destroy');
+
+    Route::post('/admin-roles/{id}/permissions', [AdminRoleController::class, 'assignPermissions'])
+        ->middleware('permission:saas-admin.admin_role.update')
+        ->name('saas-admin.admin-roles.assign-permissions');
+
+    // System Settings
+    Route::get('/system-settings', [SystemSettingController::class, 'index'])
+        ->middleware('permission:saas-admin.system_setting.view')
+        ->name('saas-admin.system-settings.index');
+
+    Route::get('/system-settings/{id}', [SystemSettingController::class, 'show'])
+        ->middleware('permission:saas-admin.system_setting.view')
+        ->name('saas-admin.system-settings.show');
+
+    Route::post('/system-settings', [SystemSettingController::class, 'upsert'])
+        ->middleware('permission:saas-admin.system_setting.update')
+        ->name('saas-admin.system-settings.upsert');
+
+    Route::delete('/system-settings/{id}', [SystemSettingController::class, 'destroy'])
+        ->middleware('permission:saas-admin.system_setting.delete')
+        ->name('saas-admin.system-settings.destroy');
 });
