5fbe076437a9e6649766bb542b190d7d99da7493
akhlaghimhd <77932814+akhlaghimhd@users.noreply.github.com>
2026-09-24 00:03:59 +0330

    feat(org-p3): UpdateBranchDTO plant-grade fields

--- app/Modules/Organization/DTOs/UpdateBranchDTO.php
+++ app/Modules/Organization/DTOs/UpdateBranchDTO.php
@@ -10,6 +10,15 @@ readonly class UpdateBranchDTO
         public ?string $companyId = null,
         public ?string $address = null,
         public bool $isActive = true,
+        public ?string $branchKind = null,
+        public bool $branchKindProvided = false,
+        public ?string $parentBranchId = null,
+        public bool $parentBranchIdProvided = false,
+        public ?string $defaultWarehouseId = null,
+        public bool $defaultWarehouseIdProvided = false,
+        public ?bool $supportsShipping = null,
+        public ?bool $supportsReceiving = null,
+        public ?bool $isManufacturingSite = null,
     ) {}
 
     public static function fromRequest(array $validatedData): self
@@ -20,6 +29,23 @@ readonly class UpdateBranchDTO
             companyId: $validatedData['company_id'] ?? null,
             address: $validatedData['address'] ?? null,
             isActive: (bool) ($validatedData['is_active'] ?? true),
+            branchKind: isset($validatedData['branch_kind'])
+                ? strtoupper((string) $validatedData['branch_kind'])
+                : null,
+            branchKindProvided: array_key_exists('branch_kind', $validatedData),
+            parentBranchId: $validatedData['parent_branch_id'] ?? null,
+            parentBranchIdProvided: array_key_exists('parent_branch_id', $validatedData),
+            defaultWarehouseId: $validatedData['default_warehouse_id'] ?? null,
+            defaultWarehouseIdProvided: array_key_exists('default_warehouse_id', $validatedData),
+            supportsShipping: array_key_exists('supports_shipping', $validatedData)
+                ? (bool) $validatedData['supports_shipping']
+                : null,
+            supportsReceiving: array_key_exists('supports_receiving', $validatedData)
+                ? (bool) $validatedData['supports_receiving']
+                : null,
+            isManufacturingSite: array_key_exists('is_manufacturing_site', $validatedData)
+                ? (bool) $validatedData['is_manufacturing_site']
+                : null,
         );
     }
 }
